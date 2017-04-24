08bfebeabdf4fa589904657af76ee979d451457a
Frezy22 <Jeppeo222@hotmail.com>
2017-04-24 17:48:44 +0200

    Update holdProcessing.php

--- docs/sites/holdProcessing.php
+++ docs/sites/holdProcessing.php
@@ -1,10 +1,11 @@
 <?php
 $hold[30][2];
+print_r($_GET);
 while($_POST['formSubmit'] == "Tilmeld")
 {
     $i++;
     $hold[$i][1] = $_GET['holdnavn'];
     $hold[$i][2] = $_GET['tlf'];
 }
-header('Location: http://zurfbeerpong.dk/sites/turneringTilmeldt.html');
+// header('Location: http://zurfbeerpong.dk/sites/turneringTilmeldt.html');
 ?>
